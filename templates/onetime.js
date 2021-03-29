631a21f7e455e3bdff3dc0b2618b08d58401092f
TonPure <35072602+TonPure@users.noreply.github.com>
2021-03-29 23:53:19 +0600

    timezone lang
    add timezone and lang options,  del dot from time data

--- templates/onetime.js
+++ templates/onetime.js
@@ -1,21 +1,26 @@
 const fs = require('fs');
 const carbone = require('carbone');
 
+carbone.set({
+  lang: "ru",
+  timezone:"Europe/Samara"
+});
+
 // Data to inject
 var data = {
   {
   "FullName": "Гребенщиков Антон Васильевич",
   "passport": "5204 997996 выдан УВД Кировского АО гОмск",
-  "DatePassport": "2004.07.02",
+  "DatePassport": "20040702",
   "inn": "084-699-854-45",
   "BankAccount": "Нужно уточнить",
   "address": "644074 Омск Конева 30-40",
   "phone": "79961210675",
   "email": "godlyton@yandex.ru",
   "req": "Реализовать генерацию договора на разработку ПО по данным из json структуры",
-  "DateContract": "2021.03.29",
-  "DateStart": "2021.03.29",
-  "DateStop": "2021.03.31",
+  "DateContract": "20210329",
+  "DateStart": "20210329",
+  "DateStop": "20210331",
   "cost": "15000",
   "cost_in_words": "Пятнадцать тысяч",
   "req1": "инициировать проект",
@@ -25,9 +30,7 @@ var data = {
   
 };
 
-// Generate a report using the sample template provided by carbone module
-// This LibreOffice template contains "Hello {d.firstname} {d.lastname} !"
-// Of course, you can create your own templates!
+
 carbone.render('./node_modules/carbone/examples/onetime.ott', data, function(err, result){
   if (err) {
     return console.log(err);
